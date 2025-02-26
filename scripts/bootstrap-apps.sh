e2940c41c35bd72a46fc67aae0c08f8ae2f2b222
surskitt <surskitt@users.noreply.github.com>
2025-02-26 17:17:41 +0000

    fix(flux): change path to point to mallard subdir

--- scripts/bootstrap-apps.sh
+++ scripts/bootstrap-apps.sh
@@ -27,7 +27,7 @@ function wait_for_nodes() {
 function apply_namespaces() {
     log debug "Applying namespaces"
 
-    local -r apps_dir="${ROOT_DIR}/kubernetes/apps"
+    local -r apps_dir="${ROOT_DIR}/kubernetes/mallard/apps"
 
     if [[ ! -d "${apps_dir}" ]]; then
         log error "Directory does not exist" "directory=${apps_dir}"
